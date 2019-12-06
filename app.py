5a4733e04db2620583aa3614549cf18a4b252ee4
Pedrenrique Guimarães <pedrenriquegg@hotmail.com>
2019-12-06 03:39:01 -0300

    Ativando CORS direto no Flask, assim não precisamos acionar o CORS no navegador.

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 import json
 from compare_json_csv import get_all
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/predictions/<int:uid>", strict_slashes=False)
 
